e20ef8676051ea7c782e8138d21707b378479da6
ranjana <ranjana.madt@gmail.com>
2021-06-18 12:58:12 +0530

    Splash Screen Added

--- app/src/main/java/com/madt/note_coding_comrades_android/activities/SplashActivity.java
+++ app/src/main/java/com/madt/note_coding_comrades_android/activities/SplashActivity.java
@@ -1,8 +1,10 @@
 package com.madt.note_coding_comrades_android.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.madt.note_coding_comrades_android.R;
 
@@ -12,5 +14,20 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this,
+                        CategoryListActivity.class);
+                //Intent is used to switch from one activity to another.
+
+                startActivity(i);
+                //invoke the SecondActivity.
+
+                finish();
+                //the current activity will get finished.
+            }
+        }, 3000);
     }
 }
